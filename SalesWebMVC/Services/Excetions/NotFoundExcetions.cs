048a0cbabd92ad565daa808f7d7d5290b7e0b096
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2022-11-17 19:52:30 -0300

    Update seller and custom service exception

---
+++ SalesWebMVC/Services/Excetions/NotFoundExcetions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Excetions
+{
+    public class NotFoundExcetions : ApplicationException
+    {
+        public NotFoundExcetions(string message) : base(message)
+        { 
+        }
+    }
+}
